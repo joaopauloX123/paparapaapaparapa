4948e2f4a803f88b4383b289dde8b8b508df43c7
joaopauloX123 <128481565+joaopauloX123@users.noreply.github.com>
2023-10-17 09:04:14 -0300

    Add files via upload

---
+++ aluramidi-curso-arquivos-iniciais/css/main.js
@@ -0,0 +1,77 @@
+// Pom ---------------------------------------
+function tocaSomPom () {
+   document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom();
+
+/*
+    Clap
+*/
+function tocaSomClap () {
+    document.querySelector('#som_tecla_clap').play();
+ }
+ 
+ document.querySelector('.tecla_clap').onclick = tocaSomClap();
+ 
+/*
+     Tim
+*/    
+ function tocaSomTim () {
+    document.querySelector('#som_tecla_tim').play();
+ }
+ 
+ document.querySelector('.tecla_tim').onclick = tocaSomTim();
+
+/*
+     Puff
+*/
+ function tocaSomPuff () {
+    document.querySelector('#som_tecla_puff').play();
+ }
+ 
+ document.querySelector('.tecla_puff').onclick = tocaSompPuff();
+
+/*
+     Splash
+*/
+ function tocaSomSplash () {
+    document.querySelector('#som_tecla_splash').play();
+ }
+ 
+ document.querySelector('.tecla_splash').onclick = tocaSomSplash();
+
+/*
+     Toim
+*/ 
+ function tocaSomToim () {
+    document.querySelector('#som_tecla_toim').play();
+ }
+ 
+ document.querySelector('.tecla_toim,').onclick = tocaSomToim();
+
+/*
+     Pish
+*/ 
+ function tocaSomPish () {
+    document.querySelector('#som_tecla_pish').play();
+ }
+ 
+ document.querySelector('.tecla_pish').onclick = tocaSomPish();
+
+/*
+     Tic
+*/ 
+ function tocaSomTic () {
+    document.querySelector('#som_tecla_tic').play();
+ }
+ 
+ document.querySelector('.tecla_tic').onclick = tocaSomTic();
+/*
+     Tom
+*/
+ function tocaSomTom () {
+    document.querySelector('#som_tecla_tom').play();
+ }
+ 
+ document.querySelector('.tecla_tom').onclick = tocaSomTom();
